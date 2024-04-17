ca055311b9351d480fddcdd559288abc46b901a7
anouar <tamagoult.anouar@gmail.com>
2024-04-17 14:56:42 +0200

    getAttribute fixed

--- src/__tests__/Bills.js
+++ src/__tests__/Bills.js
@@ -2,29 +2,30 @@
  * @jest-environment jsdom
  */
 
-import { screen, waitFor } from "@testing-library/dom"
+import { fireEvent, screen, waitFor } from "@testing-library/dom"
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
-import { ROUTES, ROUTES_PATH } from "../constants/routes.js";
-import { localStorageMock } from "../__mocks__/localStorage.js";
-import '@testing-library/jest-dom';
-import router from "../app/Router.js";
-import Bills from "../containers/Bills.js";
-import userEvent from "@testing-library/user-event";
+import { ROUTES, ROUTES_PATH } from "../constants/routes.js"
+import { localStorageMock } from "../__mocks__/localStorage.js"
+import '@testing-library/jest-dom'
+import router from "../app/Router.js"
+import Bills from "../containers/Bills.js"
+
 
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
+    Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+    window.localStorage.setItem('user', JSON.stringify({
+      type: 'Employee'
+    }))
+    const root = document.createElement("div")
+    root.setAttribute("id", "root")
+    document.body.append(root)
+    router()
+    window.onNavigate(ROUTES_PATH.Bills)
     test("Then bill icon in vertical layout should be highlighted", async () => {
 
-      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
-      window.localStorage.setItem('user', JSON.stringify({
-        type: 'Employee'
-      }))
-      const root = document.createElement("div")
-      root.setAttribute("id", "root")
-      document.body.append(root)
-      router()
-      window.onNavigate(ROUTES_PATH.Bills)
+
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
 
@@ -57,13 +58,13 @@ describe("Given I am connected as an employee", () => {
       const btnNewBill = screen.getByTestId('btn-new-bill')
       const handleClickNewBills = jest.fn((e) => billsContainer.handleClickNewBill())
       btnNewBill.addEventListener('click', handleClickNewBills)
-      btnNewBill.click()
-      
+      fireEvent.click(btnNewBill)
+
 
       expect(handleClickNewBills).toHaveBeenCalled()
 
     })
-    test("Ensuite, cliquez sur l'icône de l'œil", async () => {
+    test("Then click on eye icon", async () => {
       // Définir une propriété localStorage mockée sur l'objet window
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       // Définir une valeur dans le localStorage
@@ -78,10 +79,10 @@ describe("Given I am connected as an employee", () => {
       router()
       // Simuler le changement de route vers la page des factures
       window.onNavigate(ROUTES_PATH.Bills)
-    
+
       // Injecter le HTML de la page des factures dans le document body
       document.body.innerHTML = BillsUI({ data: bills })
-    
+
       // Définir une fonction onNavigate mockée
       const onNavigate = (pathname) => {
         document.body.innerHTML = ROUTES({ pathname })
@@ -92,34 +93,26 @@ describe("Given I am connected as an employee", () => {
       const billsContainer = new Bills({
         document, onNavigate, store, bills, localStorage: window.localStorage
       })
-    
+
       // Attendre que l'icône de l'œil soit rendue à l'écran
       await waitFor(() => screen.getByTestId('btn-new-bill'))
       window.$ = jest.fn().mockImplementation(() => {
         return {
-           modal:jest.fn(),
-           width:jest.fn(),
-           find: jest.fn().mockImplementationOnce(() =>{return {html: jest.fn() }})
-        
-         }
-     });
-      //await waitFor(() => screen.findAllByTestId('icon-eye'))
+          modal: jest.fn(),
+          width: jest.fn(),
+          find: jest.fn().mockImplementationOnce(() => { return { html: jest.fn() } })
+
+        }
+      });
       // Récupérer toutes les icônes de l'œil
       const eyeIcons = screen.getAllByTestId("icon-eye");
-      // Cliquer sur chaque icône de l'œil
-    //  eyeIcons.forEach((eyeIcon) => {
-    //    eyeIcon.click()
-     // });  
-    
-      const handleClickIconEyes = jest.fn((e) =>billsContainer.handleClickIconEye(e))
-       
-     // eyeIcons[0].getAttribute=jest.fn((e) =>eyeIcons[0].getAttribute())
-    eyeIcons[0].addEventListener("click", handleClickIconEyes)
-    eyeIcons[0].click()
+      const handleClickIconEyes = jest.fn(() => billsContainer.handleClickIconEye(eyeIcons[0]));
+      eyeIcons[0].addEventListener("click", handleClickIconEyes);
+      fireEvent.click(eyeIcons[0])
       // Vérifier si handleClickIconEyes est appelée après chaque clic sur une icône de l'œil
       expect(handleClickIconEyes).toHaveBeenCalled()
     })
-    
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -129,3 +122,33 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+// test d'intégration GET
+//describe("Given I am a user connected as Employee", () => {
+  //describe("When I navigate to Bills", () => {
+   // test("fetches bills from mock API GET", async () => {
+
+      //Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+   //   window.localStorage.setItem('user', JSON.stringify({
+      //  type: 'Employee'
+     // }))
+
+     // const root = document.createElement("div")
+     // root.setAttribute("id", "root")
+     // document.body.append(root)
+    //  router()
+     // window.onNavigate(ROUTES_PATH.Bills)
+     // await waitFor(() => screen.getAllByTestId("tbody"))
+
+      // Obtenir toutes les factures affichées sur la page
+      //const billElements = screen.getAllByTestId("tbody")
+
+      // Vérifier si chaque facture est affichée avec  le statut formatés
+     // billElements.forEach((billElement, index) => {
+      //  const billData = bills[index]
+
+       // expect(billElement).toHaveTextContent(billData.status)
+     // })
+   // })
+  //})
+//})
+
--- src/containers/Bills.js
+++ src/containers/Bills.js
@@ -7,7 +7,7 @@ export default class {
     this.document = document
     this.onNavigate = onNavigate
     this.store = store
-    const buttonNewBill =document.querySelector(`button[data-testid="btn-new-bill"]`)
+    const buttonNewBill = document.querySelector(`button[data-testid="btn-new-bill"]`)
     if (buttonNewBill) buttonNewBill.addEventListener('click', this.handleClickNewBill)
     const iconEye = document.querySelectorAll(`div[data-testid="icon-eye"]`)
     if (iconEye) iconEye.forEach(icon => {
@@ -28,33 +28,33 @@ export default class {
   }
 
   getBills = () => {
-    if (this.store) {
+        if (this.store) {
       return this.store
-      .bills()
-      .list()
-      .then(snapshot => {
-        const bills = snapshot
-          .map(doc => {
-            try {
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
+        .bills()
+        .list()
+        .then(snapshot => {
+          const bills = snapshot
+            .map(doc => {
+              try {
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
+              } catch (e) {
+                // if for some reason, corrupted data was introduced, we manage here failing formatDate function
+                // log the error and return unformatted date in that case
+                console.log(e, 'for', doc)
+                return {
+                  ...doc,
+                  date: doc.date,
+                  status: formatStatus(doc.status)
+                }
               }
-            } catch(e) {
-              // if for some reason, corrupted data was introduced, we manage here failing formatDate function
-              // log the error and return unformatted date in that case
-              console.log(e,'for',doc)
-              return {
-                ...doc,
-                date: doc.date,
-                status: formatStatus(doc.status)
-              }
-            }
-          })
+            })
           console.log('length', bills.length)
-        return bills
-      })
+          return bills
+        })
     }
   }
 }
